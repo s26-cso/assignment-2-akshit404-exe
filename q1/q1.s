889488812f06fe486190d9370fa2aa21e5e9b383
akshit404-exe <akshit.singla@students.iiit.ac.in>
2026-04-09 23:00:53 +0530

    Solved q1
    Implemented functions for creating and manipulating a binary search tree.

---
+++ q1/q1.s
@@ -0,0 +1,110 @@
+.global make_node
+.global insert
+.global get
+.global getAtMost
+make_node:
+    addi sp,sp,-16
+    sd ra, 8(sp)
+    sw a0, 4(sp)
+
+    li a0, 24
+    call malloc
+
+    lw t0, 4(sp)
+    sw t0, 0(a0)
+
+    addi t0, x0, 0
+    sd t0, 8(a0)
+    sd t0, 16(a0)
+
+    ld ra, 8(sp)
+    addi sp, sp, 16
+    ret
+
+
+
+insert:
+    addi sp, sp, -24
+    sd ra, 16(sp)
+    sd a0, 8(sp)      
+    sw a1, 4(sp) 
+    beq zero, a0, create_node
+
+    lw t0, 0(a0)
+
+    blt a1, t0, left
+
+    ld t1, 16(a0)
+    mv a0, t1
+    lw a1, 4(sp)
+    call insert
+    ld t2, 8(sp)
+    sd a0, 16(t2)     
+    mv a0, t2
+    j done
+
+left:
+    ld t1, 8(a0)
+    mv a0, t1
+    lw a1, 4(sp)
+    call insert
+    ld t2, 8(sp)
+    sd a0, 8(t2)     
+    mv a0, t2
+    j done
+
+create_node:
+    lw a0, 4(sp)
+    call make_node
+
+done:
+    ld ra, 16(sp)
+    addi sp, sp, 24
+    ret
+
+
+
+get:
+    beq zero,a0, not_found
+
+loop:
+    lw t0, 0(a0)
+
+    beq t0, a1, found 
+    blt t0, a1, right 
+    ld a0, 8(a0)
+    beq a0, zero, not_found
+    j loop
+right:
+    ld a0, 16(a0)
+    beq a0, zero, not_found
+    j loop
+found:
+    ret
+not_found:
+    li a0, 0
+    ret
+
+
+
+getAtMost:
+    li t0, -1          
+
+loop2:
+    beq a1, zero, done_most
+
+    lw t1, 0(a1)
+
+    blt a0, t1, go_left
+
+    mv t0, t1
+    ld a1, 16(a1)      
+    j loop2
+
+go_left:
+    ld a1, 8(a1)
+    j loop2
+
+done_most:
+    mv a0, t0
+    ret
